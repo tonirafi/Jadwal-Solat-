2b28550ca951c080a476b38d22fe5509d96918f6
tony rafi <toni@pengembangan.lakuin.id>
2020-10-21 10:16:28 +0700

    cek next time

--- app/src/main/java/com/sirem/jadwalsolat/ui/home/HomeFragment.kt
+++ app/src/main/java/com/sirem/jadwalsolat/ui/home/HomeFragment.kt
@@ -9,15 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sirem.jadwalsolat.R
 import com.sirem.jadwalsolat.service.AlarmReceiver
 import com.sirem.jadwalsolat.service.ReminderNotificationService
 import com.sirem.jadwalsolat.ui.adapter.ListPrayAdapter
-import kotlinx.android.synthetic.main.fragment_jadwal_solat.*
-import kotlinx.android.synthetic.main.item_time_pray.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -31,7 +30,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return   inflater.inflate(R.layout.fragment_jadwal_solat, container, false)
+        return   inflater.inflate(R.layout.fragment_home, container, false)
 
     }
 
@@ -42,15 +41,64 @@ class HomeFragment : Fragment() {
 
     fun setUi(){
 
-
         adapter = context?.let { ListPrayAdapter(it)}!!
-
         val linearLayoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = adapter
         adapter.addAll(getDataPray())
         setAlarm(getDataPray())
+        tv_date.text=getDateLocal("MMMM dd")
+        tv_time.text=getDateLocal("hh:mm:ss")
+        tv_nextTime.text="Next Prayer "+cekNextTime(getDataPray())
+
     }
+
+
+
+    private fun setAlarm(list:ArrayList<DataAzan>) {
+        showForeground()
+        list.forEachIndexed { index, dataAzan ->
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, dataAzan.jam!!)
+            calendar.set(Calendar.MINUTE, dataAzan.menit!!)
+            var time = calendar.timeInMillis - calendar.timeInMillis % 60000
+
+            val intent = Intent(context, AlarmReceiver::class.java)
+            intent.putExtra("title","Sudah Masuk Waktu ${dataAzan.namaWaktu}")
+            intent.putExtra("showForeground",false)
+            intent.putExtra("des","Solat Dulu Yuk")
+            val intent_id = System.currentTimeMillis().toInt()
+            var pendingIntent = PendingIntent.getBroadcast(context, intent_id, intent,  PendingIntent.FLAG_UPDATE_CURRENT)
+            if (System.currentTimeMillis() > time) {
+                if (Calendar.AM_PM === 0)
+                    time += 1000 * 60 * 60 * 12
+                else
+                    time += time + 1000 * 60 * 60 * 24
+            }
+
+            var alarmManager = context?.getSystemService(ALARM_SERVICE) as AlarmManager
+            alarmManager!!.setInexactRepeating(AlarmManager.RTC_WAKEUP, time, AlarmManager.INTERVAL_DAY, pendingIntent);
+        }
+
+    }
+
+
+    fun showForeground(){
+        Toast.makeText(context, "Pengingat Waktu Solat Aktif", Toast.LENGTH_SHORT).show()
+        val i = Intent(context, ReminderNotificationService::class.java)
+        context?.stopService(i)
+        i.putExtra("title","Pengingat Waktu Solat Aktif")
+        i.putExtra("showForeground",true)
+        i.putExtra("des","")
+        context?.startService(i)
+    }
+
+    fun getDateLocal(format:String): String {
+        val sdf = SimpleDateFormat(format)
+        val currentDate = sdf.format(Date())
+        return currentDate
+    }
+
 
 
     fun getDataPray():ArrayList<DataAzan>{
@@ -95,43 +143,44 @@ class HomeFragment : Fragment() {
         return list
     }
 
+    fun cekNextTime(list: ArrayList<DataAzan>) :String{
 
+        val date:Date = Date()
+        val cal = Calendar.getInstance()
+        cal.time = date
+        var namePray=""
+        var time=""
+        val hours = cal.get(Calendar.HOUR_OF_DAY)
+        when {
+            list.get(0).jam!! > hours -> {
+                 namePray= list.get(0).namaWaktu.toString()
+                time="${list.get(0).jam}:${list.get(0).menit}"
+            }
+            list.get(1).jam!! > hours -> {
+                namePray= list.get(1).namaWaktu.toString()
+                time="${list.get(1).jam}:${list.get(1).menit}"
 
-    private fun setAlarm(list:ArrayList<DataAzan>) {
-        showForeground()
-        list.forEachIndexed { index, dataAzan ->
-            val calendar = Calendar.getInstance()
-            calendar.set(Calendar.HOUR_OF_DAY, dataAzan.jam!!)
-            calendar.set(Calendar.MINUTE, dataAzan.menit!!)
-            var time = calendar.timeInMillis - calendar.timeInMillis % 60000
-
-            val intent = Intent(context, AlarmReceiver::class.java)
-            intent.putExtra("title","Sudah Masuk Waktu ${dataAzan.namaWaktu}")
-            intent.putExtra("showForeground",false)
-            intent.putExtra("des","Solat Dulu Yuk")
-            val intent_id = System.currentTimeMillis().toInt()
-            var pendingIntent = PendingIntent.getBroadcast(context, intent_id, intent,  PendingIntent.FLAG_UPDATE_CURRENT)
-            if (System.currentTimeMillis() > time) {
-                if (Calendar.AM_PM === 0)
-                    time += 1000 * 60 * 60 * 12
-                else
-                    time += time + 1000 * 60 * 60 * 24
+            }
+            list.get(2).jam!! > hours -> {
+                namePray= list.get(2).namaWaktu.toString()
+                time="${list.get(2).jam}:${list.get(2).menit}"
+            }
+            list.get(3).jam!! > hours -> {
+                namePray= list.get(3).namaWaktu.toString()
+                time="${list.get(3).jam}:${list.get(3).menit}"
+            }
+            list.get(4).jam!! > hours -> {
+                namePray= list.get(4).namaWaktu.toString()
+                time="${list.get(4).jam}:${list.get(4).menit}"
+            }
+            list.get(5).jam!! > hours -> {
+                namePray= list.get(5).namaWaktu.toString()
+                time="${list.get(5).jam}:${list.get(5).menit}"
             }
 
-            var alarmManager = context?.getSystemService(ALARM_SERVICE) as AlarmManager
-            alarmManager!!.setInexactRepeating(AlarmManager.RTC_WAKEUP, time, AlarmManager.INTERVAL_DAY, pendingIntent);
         }
 
+        return "$namePray $time"
     }
 
-
-    fun showForeground(){
-        Toast.makeText(context, "Pengingat Waktu Solat Aktif", Toast.LENGTH_SHORT).show()
-        val i = Intent(context, ReminderNotificationService::class.java)
-        context?.stopService(i)
-        i.putExtra("title","Pengingat Waktu Solat Aktif")
-        i.putExtra("showForeground",true)
-        i.putExtra("des","")
-        context?.startService(i)
-    }
 }
